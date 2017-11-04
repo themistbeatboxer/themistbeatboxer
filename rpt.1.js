760ff19e21978b884330d3055cf4fb9736c36976
themistbeatboxer <33383032+themistbeatboxer@users.noreply.github.com>
2017-11-04 19:24:23 -0400

    Add files via upload

---
+++ rpt.1.js
@@ -0,0 +1,14 @@
+window.onload = function()
+{
+	var constraints = {audio: true, video: true};
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream)
+	{
+		var video = document.querySelector('video');
+		video.srcObject = mediaStream;
+		video.play(); 
+	}).catch(function(err)
+	{
+		console.log("Error " + err.message);
+	})
+}
